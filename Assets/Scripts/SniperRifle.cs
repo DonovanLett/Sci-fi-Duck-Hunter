a4083a1f29aed8f2385e79fb064e7ef169784a9d
Donovan Lett <donovanlett85@gmail.com>
2025-12-29 11:16:24 -1000

    Sniper Rifle Script Added

---
+++ Assets/Scripts/SniperRifle.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+
+public class SniperRifle : MonoBehaviour
+{
+    private PlayerInputActions _playerInput;
+
+    [SerializeField]
+    private LayerMask _enemyMask;
+
+    [SerializeField]
+    private int _ammo;
+
+    [SerializeField]
+    private int _ammoCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ammoCount = _ammo;
+    }
+
+    private void OnEnable()
+    {
+        _playerInput = new PlayerInputActions();
+        _playerInput.SniperRifle.Enable();
+        _playerInput.SniperRifle.Fire.performed += Fire;
+    }
+
+    private void Fire(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        if (_ammoCount > 0)
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, Mathf.Infinity, _enemyMask))
+            {
+                if (hitInfo.collider.tag == "Duck" && hitInfo.collider.GetComponent<Duck_AI>() != null)
+                {
+                    hitInfo.collider.GetComponent<Duck_AI>().OnShot();
+                }
+            }
+            _ammoCount--;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.SniperRifle.Fire.performed -= Fire;
+        _playerInput.SniperRifle.Disable();
+    }
+}
